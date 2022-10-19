56778e03d4482b2cb4769ff6fe639fb618d93b99
cocy <zuhthebeg@naver.com>
2022-10-19 16:23:47 +0900

    update 2022_2

--- 2021_sec_edu.js
+++ 2021_sec_edu.js
@@ -6,49 +6,41 @@
 $('.btn_small').each(function(a,d){
     console.log($(d)[0].outerHTML.substring(39,75));
 })
-openLectureRoom(6,3340,'B051',2516);
-VM10523:2 openLectureRoom(1,3428,'B051',2619);
-VM10523:2 openLectureRoom(1,3427,'B051',2620);
-VM10523:2 openLectureRoom(1,3437,'B051',2612);
-VM10523:2 openLectureRoom(1,3436,'B051',2613);
 
-
+openLectureRoom(1,3391,'B051',2577);
+openLectureRoom(1,3297,'B051',2475);
+openLectureRoom(4,3322,'B051',2497);
+openLectureRoom(3,3321,'B051',2498);
 
 2. 강의페이지까지 들어가서 아래 코드 실행 (by jax)
 Array.from(document.querySelectorAll('script')).at(-1).innerText.match(/\"contents_seq\":(\d)+\,/g).map(v => parseInt(v.replace(/[^0-9]/g, '')))
 */
 
-	let classData_2021_2 = [
+	let classData_2022_2 = [
 	{
-		class_seq: "6",
-		course_code: "3340",
+		class_seq: "1",
+		course_code: "3391",
 		site_id: "B051",
-		subject_code: "2516",
-		contents_seq: [144930, 144931, 144932, 144933, 144934, 144935, 144936, 144937, 144938, 144939, 144940, 144932]
+		subject_code: "2577",
+		contents_seq: [145324, 145325, 145328, 145329, 145330, 145331, 145332, 145333, 145334, 145328]
 	},{
 		class_seq: "1",
-		course_code: "3428",
+		course_code: "3297",
 		site_id: "B051",
-		subject_code: "2619",
-		contents_seq: [146540, 146541, 146542, 146543, 146544, 146545, 146546, 146547, 146548, 146549, 146550, 146551, 146552, 146553, 146542]
+		subject_code: "2475",
+		contents_seq: [144449, 144450, 144451, 144452, 144453, 144454, 144455, 144456, 144457, 144458, 144459, 144451]
 	},{
-		class_seq: "1",
-		course_code: "3427",
+		class_seq: "4",
+		course_code: "3322",
 		site_id: "B051",
-		subject_code: "2620",
-		contents_seq: [146554, 146555, 146556, 146557, 146558, 146559, 146560, 146561, 146562, 146563, 146564, 146565, 146566, 146567, 146568, 146569, 146556]
+		subject_code: "2497",
+		contents_seq: [144705, 144706, 144707, 144708, 144709, 144710, 144711, 144712, 144713, 144714, 144715, 144716, 144717, 144707]
 	},{
-		class_seq: "1",
-		course_code: "3437",
+		class_seq: "3",
+		course_code: "3321",
 		site_id: "B051",
-		subject_code: "2612",
-		contents_seq: [146448, 146449, 146450, 146451, 146452, 146453, 146454, 146455, 146456, 146457, 146458, 146459, 146450]
-	},{
-		class_seq: "1",
-		course_code: "3436",
-		site_id: "B051",
-		subject_code: "2613",
-		contents_seq: [146460, 146461, 146462, 146463, 146464, 146465, 146466, 146467, 146468, 146469, 146470, 146471, 146472, 146473, 146462]
+		subject_code: "2498",
+		contents_seq: [144718, 144719, 144720, 144721, 144722, 144723, 144724, 144725, 144726, 144727, 144728, 144729, 144730, 144720]
 	}
 
 
@@ -57,7 +49,7 @@ Array.from(document.querySelectorAll('script')).at(-1).innerText.match(/\"conten
 	];
 
 	let extractedFormData = [];
-	classData_2021_2.map(classItem => {
+	classData_2022_2.map(classItem => {
 		let classFormdata = JSON.parse(JSON.stringify(classItem));
 		classItem.contents_seq.map(contents_seq => {
 			classFormdata.contents_seq = contents_seq;
